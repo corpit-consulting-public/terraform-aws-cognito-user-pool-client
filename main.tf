840a1e420e985f1d1bc8f40e96f5f74dfdc9a756
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-02-04 17:00:06 -0300

    deleted variable optional

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_cognito_user_pool_client" "client" {
   name = "${var.name}"
   user_pool_id = "${var.user_pool_id}"
-  generate_secret                      = "${var.generate_secret}"
   explicit_auth_flows                  = ["${var.explicit_auth_flows}"]
   read_attributes                      = "${var.read_attributes}"
   write_attributes                     = "${var.write_attributes}"
