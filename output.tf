80103fb2a0f751aa7f87251b72201b81cc309947
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-06-03 18:06:41 -0300

    tf12 upgrade

--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
 output "id" {
   description = "The id of the user pool client"
-  value       = "${aws_cognito_user_pool_client.client.id}"
+  value       = aws_cognito_user_pool_client.client.id
 }
+
